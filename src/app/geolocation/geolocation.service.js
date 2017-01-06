8e20c8729fb59fa91ca68c3932e9bd0d7f5dc676
uros pajic <uros pajic>
2017-01-06 12:06:45 +0100

    Added random gps generator for belgrade jobs if the gelocation service is not available (bitballoon)

--- src/app/geolocation/geolocation.service.js
+++ src/app/geolocation/geolocation.service.js
@@ -17,9 +17,9 @@
         //////////////////////////////
 
         function init() {
-            var posOptions = { 
-                timeout: 10000, 
-                enableHighAccuracy: false 
+            var posOptions = {
+                timeout: 10000,
+                enableHighAccuracy: false
             };
             $cordovaGeolocation
                 .getCurrentPosition(posOptions)
@@ -64,10 +64,40 @@
         }
 
         function setFallbackCoordinates() {
+            var randomCoords = randomGeo({
+                latitude: 44.802433, 
+                longitude: 20.466403
+            }, 10000);
             _currentLocation = new GeolocationModel({
-                latitude: 44,
-                longitude: 20
+                latitude: parseFloat(randomCoords.latitude),
+                longitude: parseFloat(randomCoords.longitude)
             });
+        }
+
+        function randomGeo(center, radius) {
+            var y0 = center.latitude;
+            var x0 = center.longitude;
+            var rd = radius / 111300; //about 111300 meters in one degree
+
+            var u = Math.random();
+            var v = Math.random();
+
+            var w = rd * Math.sqrt(u);
+            var t = 2 * Math.PI * v;
+            var x = w * Math.cos(t);
+            var y = w * Math.sin(t);
+
+            //Adjust the x-coordinate for the shrinking of the east-west distances
+            var xp = x / Math.cos(y0);
+
+            var newlat = y + y0;
+            var newlon = x + x0;
+            var newlon2 = xp + x0;
+
+            return {
+                latitude: newlat.toFixed(8),
+                longitude: newlon.toFixed(8)
+            };
         }
     }
 })();
